19c37235b8f2a8fa7e735914b094e5057bc17fc1
Sunat.P <31818562+SunatP@users.noreply.github.com>
2018-05-02 15:45:34 +0700

    Update Select Seat

--- Project/project-cinema.c
+++ Project/project-cinema.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <windows.h>
 #include <string.h>
-
+#include<stdlib.h>
+#include<time.h>
     struct Movie
 {
     char title[30];
     int yearMovie;
+    float hr;
+    float min;
+    float stime;
+    int screen;
 };
-struct Movie Title[10] ={{"The Transporter",2002},//15
-    {"Transpoter2", 2005},//11
-    {"Transformers", 2007},//12
-    {"Transformers : The Last Knight", 2017},//30
-    {"Lost in Translation", 2003},//19
-    {"Ready player one", 2018},//16
-    {"Jumanji", 2018},//7
-    {"Infinity Avengers Part I", 2018},//24
-    {"The Matrix", 1999},//10
-    {"up", 2008}};
+struct Movie Title[100] ={{"The Transporter",2002,1,32,10.00,1},//15
+    {"Transpoter2", 2005,1,27,13.00,1},//11
+    {"Transformers", 2007,2,24,10.00,2},//12
+    {"Transformers : The Last Knight", 2017,2,34,13.00,2},//30
+    {"Lost in Translation", 2003,1,42,11.00,3},//19
+    {"Ready player one", 2018,1,15,14.00,1},//16
+    {"Jumanji", 2018,1,32,15.00,1},//7
+    {"Infinity Avengers Part I", 2018,1,37,16.00,1},//24
+    {"The Matrix", 1999,1,18,13.00,1},//10
+    {"up", 2008,1,35,17.00,1}};
 void search()
 {
     struct Movie mov;
@@ -31,9 +36,9 @@ void search()
     printf("Please input Title for searching : ");
 
     scanf("%s",&ti);
-    printf("=================================================================================\n");
-    printf("      Title             Year        Length      ShowTime        Screen\n");
-    printf("=================================================================================\n");
+    printf("=================================================================================================================\n");
+    printf("      Title                              Year            Length         ShowTime      Screen\n");
+    printf("=================================================================================================================\n");
     for(i=0;i<10;i++)
     {
         strcpy(keep,Title[i].title);
@@ -42,31 +47,39 @@ void search()
         {
             printf("[%d]",i+1);
 
-            printf(" %-15s \t%5d  \n",Title[i].title,Title[i].yearMovie);
+            printf(" %-35s\t%5d  \t\t%.0fh %.0fmin \t%.2f \t\t%d\n",Title[i].title,Title[i].yearMovie,Title[i].hr,Title[i].min,Title[i].stime,Title[i].screen);
             continue;
         }
 
         count++;
     }
-   printf("=================================================================================\n");
+   printf("=================================================================================================================\n");
     int asd,enter;
     char y,q;
     printf("What movie do you want to watch?: ");
     scanf("%d",&asd); // รับค่าเป็นตัวเลข
+
+
+            printf("[%s]\n",Title[asd-1].title);
+            printf("Title: %s\n",Title[asd-1].title);
+            printf("Year: %d\n", Title[asd-1].yearMovie);
+            printf("Duration: %.0fh %.0fmin\n",Title[asd-1].hr,Title[asd-1].min);
+            printf("Time: %.2f - %.2f\n",  Title[asd-1].stime,(Title[asd-1].stime+Title[asd-1].hr+(Title[asd-1].min/100)));
+            printf("Screen: %d\n",Title[asd-1].screen);
+
         printf("Select %s (Yes/No):? ",Title[asd-1].title); // เลือกหนัง
         scanf("%s",&y);
         if(y=='Y'||y =='y'){
-            printf("[%s]\n",Title[asd].title);
-            printf("Title: %s\n",Title[asd].title);
-            printf("Year: %d", Title[asd].yearMovie);
+            Sleep(1500);
+            seat();
         }
         if(y=='n' || y=='N')
             {
-                printf("Return To Search And Select Again if press N program will go Main Menu? (Y/N): ");
+                printf("Return To Search And Select Again if press N and Enter program will go Main Menu? (Y/N): ");
                  scanf("%s",&q);
                 if(q=='Y'||q =='y')
                 {
-                 system("cls");
+
                  search();
                 }
                 else {
@@ -75,6 +88,8 @@ void search()
                                 enter = fgetc(stdin);
                     if(enter == 0x0A) // กดปุ่ม Enter เพื่อจบการทำงาน
                     {
+                        printf("Program will go to main Menu");
+                        Sleep(1500);
                         system("cls");
                     fprintf(stdout,main());
                     }
@@ -82,6 +97,69 @@ void search()
                 }
             }
     }
+int seat()
+{
+    int i,j,k;
+    int xx;
+    xx= "X";
+    float x;
+    char y;
+    char board[4][10] = {
+         {'O','O','O','O','O','O','O','O','O','O'},
+         {'O','X','O','O','O','X','O','O','O','O'},
+         {'O','O','O','O','O','O','O','X','X','O'},
+         {'O','O','O','O','X','X','O','O','O','O'}};
+    printf("===============================\n");
+    printf("Available Seat\n");
+    printf("===============================\n");
+    printf("Row a: ");
+    for( i = 0; i<1; i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            printf("|%c",board[i][j]);
+        }
+        printf("|");
+    }
+    printf("\nRow b: ");
+    for( i = 1; i<2; i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            printf("|%c",board[i][j]);
+        }
+        printf("|");
+    }
+    printf("\nRow c: ");
+    for( i = 2; i<3; i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            printf("|%c",board[i][j]);
+        }
+        printf("|");
+    }
+     printf("\nRow d: ");
+    for( i = 3; i<4; i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            printf("|%c",board[i][j]);
+        }
+        printf("|");
+    }
+    do{
+          printf("\nHow many tickets do you want to buy? ");
+          scanf("%d", &k);
+          printf("\n");
+          
+}
+
+
+
+
+
+
 
 int showtime()
 {
